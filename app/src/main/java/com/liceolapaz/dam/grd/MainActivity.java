102220972d700b521c9a468a5caafeab71f972ed
Gabriel <correoclasegabri@gmail.com>
2020-10-14 13:20:34 +0200

    GUI Login

---
+++ app/src/main/java/com/liceolapaz/dam/grd/MainActivity.java
@@ -0,0 +1,22 @@
+package com.liceolapaz.dam.grd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
+    }
+}
