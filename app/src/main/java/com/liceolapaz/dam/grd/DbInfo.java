ac49d200767328c2ab91cb39696f690f14975e88
GabriRDiaz <correoclasegabri@gmail.com>
2020-10-16 09:09:17 +0200

    Button Working!

--- app/src/main/java/com/liceolapaz/dam/grd/DbInfo.java
+++ app/src/main/java/com/liceolapaz/dam/grd/DbInfo.java
@@ -3,6 +3,8 @@ package com.liceolapaz.dam.grd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class DbInfo extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class DbInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_db_info);
+    }
+
+    public void addPlayer(View view) {
+        Toast.makeText(this, "Button Working!", Toast.LENGTH_SHORT).show();
     }
 }
