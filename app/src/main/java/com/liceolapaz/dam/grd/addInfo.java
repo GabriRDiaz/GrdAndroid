bc611f5444444bed30b6fff418547c2423d9e430
Gabriel <correoclasegabri@gmail.com>
2020-10-23 19:30:13 +0200

    Dials Working!

--- app/src/main/java/com/liceolapaz/dam/grd/addInfo.java
+++ app/src/main/java/com/liceolapaz/dam/grd/addInfo.java
@@ -2,11 +2,14 @@ package com.liceolapaz.dam.grd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.FragmentManager;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 
 import static android.app.PendingIntent.getActivity;
@@ -35,23 +38,49 @@ public class addInfo extends AppCompatActivity {
         addPos.setAdapter(adapter);
     }
 
-
     public void cancelAddInfo(View view) {
-        txtDialog="Changes will be discarted";
+        txtDialog="Data will be discarted";
         titleDialog="Cancel";
-
+        createDialog(txtDialog, titleDialog);
     }
 
     public void deleteInfo(View view) {
         txtDialog="Data will be removed from database";
         titleDialog="Delete";
-
+        createDialog(txtDialog, titleDialog);
     }
 
     public void acceptAddInfo(View view) {
         txtDialog="Data will be saved in database";
         titleDialog="Accept";
+        createDialog(txtDialog, titleDialog);
     }
-    
+    private void createDialog(String txtDialog, String titleDialog){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(titleDialog);
+        builder.setMessage(txtDialog);
+        builder.setPositiveButton("Accept", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(addInfo.this, "Accept", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(addInfo.this, "Neutral", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(addInfo.this, "Deny", Toast.LENGTH_SHORT).show();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
 
 }
